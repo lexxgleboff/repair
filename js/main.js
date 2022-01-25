9a4842e9dcdb5a2978deaa93690bc642f455bebf
lexxgleboff <64305994+lexxgleboff@users.noreply.github.com>
2022-01-25 11:34:03 +0300

    Сверстал блок слайдера

--- js/main.js
+++ js/main.js
@@ -10,6 +10,7 @@
 //    modal.classList.remove('modal_active')
 // });
 
+// Открытие и закрытие модального окна обратной связи
 $(function() {
     $('#button').on('click', function () {
         $('#modal').addClass('modal_active');
@@ -17,4 +18,52 @@ $(function() {
     $('#close').on('click', function () {
         $('#modal').removeClass('modal_active'); 
     });
+
+    // Обработка и отправка формы через AJAX
+    $('#offer-form').on('submit', function (event) {
+        // отключаем стандартное поведение отправки формы
+        event.preventDefault();
+        const formURL = $(this).attr('action');
+        $.ajax({
+            url: formURL,
+            type: 'POST',
+            data: $(this).serialize(),
+            success: function (data) {
+                $('.success').css('display', 'flex');
+                $('.success-dialog__title').text(data + ', ваша форма отправлена!');
+                setTimeout(function () {
+                    $('.success').css('display', 'none')
+                }, 3000);
+            },
+            error: function (jqXHR, textStatus) {
+                console.log(jqXHR + ': ' + textStatus);
+            }
+            });
+    });
+    
+    $('.slider').slick({
+        infinite: true,
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        prevArrow: $('.arrows__left'),
+        nextArrow: $('.arrows__right'),
+        responsive: [
+            {
+              breakpoint: 1201,
+              settings: {
+                slidesToShow: 2,
+                slidesToScroll: 1,
+              }
+            },
+            {
+              breakpoint: 768,
+              settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1
+              }
+            }
+          ]
+    });
 });
+
+
